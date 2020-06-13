629ee627defb32d0b47bd8e3ba42b7dfd2186ee7
Tellvinch <tellvinchimani@gmail.com>
2020-06-13 11:09:54 +0300

    deployment to heroku again

--- manage.py
+++ manage.py
@@ -8,7 +8,7 @@ from flask_script import Manager,Server
 
 # Creating app instance
 # app = create_app('test')
-app = create_app('development')
+app = create_app('production')
 # app = create_app('production')
 
 
