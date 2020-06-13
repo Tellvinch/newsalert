28bdc011523fdbb14c9d7f931883e8a2316ca5a5
Tellvinch <tellvinchimani@gmail.com>
2020-06-13 10:06:31 +0300

    add changes

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-export NEWS_API_KEY='9559ec80331845ac90b4e044b5b858ee'
-export SECRET_KEY = '1248226'
+export NEWS_API_KEY='0ef96b496eb7419e8c763bdf082bea06'
+export SECRET_KEY = '142297'
 
 
 python3.6 manage.py server 
